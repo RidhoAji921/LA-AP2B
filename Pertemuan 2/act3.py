2dfd50c852033cc535c10f4e96dd9a3892cc8a00
Ridho Aji <ridhoaji921@gmail.com>
2023-04-10 12:47:48 +0700

    pertemuan 2

---
+++ Pertemuan 2/act3.py
@@ -0,0 +1,13 @@
+import requests
+
+url = 'http://localhost:3000/users'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print("ERROR: ", response.status_code)
+
+#output di web dan output python dua duanya harus sama
